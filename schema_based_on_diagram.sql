c5a252a49dd2400677ff2e5548fd48f56be4bad8
Hamza <hamzaoutdoors@gmail.com>
2021-11-02 23:21:23 +0100

    feat: Add join tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -23,3 +23,25 @@ CREATE TABLE invoices (
     FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
     PRIMARY KEY (id)
 );
+
+
+CREATE TABLE medical_histories  (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    admitted_at DATE, 
+    patient_id INT,    
+    status VARCHAR(255),
+    FOREIGN KEY (patient_id) REFERENCES patients(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE invoice_items(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    unit_price DECIMAL,  
+    quantity INT,
+    total_price DECIMAL, 
+    invoice_id INT,    
+    treatment_id INT,            
+    FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+    FOREIGN KEY (treatment_id) REFERENCES treatments(id),    
+    PRIMARY KEY (id)
+);
